6b235730a6675bd657d9b15cd369563ff77326f4
Amaya <guillaume.le-berre@epitech.eu>
2019-11-03 17:43:11 +0100

    [UP] algorithm for complex arch && doc about complex arch file formating

--- src/Core.cpp
+++ src/Core.cpp
@@ -62,20 +62,57 @@ bool Core::architectCode()
 
 	for (const std::string &s: arch) {
 		(void)s;
-		/// string current_name (cf 1)
-		/// string current_path (cf 2)
+		/// 1 - Complete partials lines in string of vector
+		/// if a line is empty delete it
+		///
+		/// Example :
+		/// Item - Potion - Heal
+		///               - Damage
+		///(empty line)
+		/// Into :
+		/// *Project Name* - inc - Item - Potion - Heal
+		/// *Project Name* - src - Item - Potion - Heal
+		/// *Project Name* - inc - Item - Potion - Damage
+		/// *Project Name* - src - Item - Potion - Damage
+		///
+		/// Algorithmic pseudo code :
+		/// If the current line is empty reset ressource and take the next line for ressources
+		/// Take the last '-' position on the line above
+		/// Take all the char before (we'll call it : std::string midle)
+		/// Take the current line delete all the ' ' (we'll call it : std::string end)
+		/// Push it into the vector will be return as :
+		/// *Project name* + "inc" + middle + end
+		/// *Project name* + "src" + middle + end
 
-		/// 1 - get name of current level of architecture (from left to right)
-		/// always have the current level of arch in a string
-		/// maybe create a function that find the current and set the next one in return value
+		/// 2 - Delete all spaces & replace all '-' by '/'
+		/// By for loop range
 
-		/// 2 - if needed create the folder
-		/// so we have to have the current full path of the architecture level in a string
-		/// so if we get deeper in the string we have to += of
-		/// there will always be directories for this
+		/// 3 - Send the current line to a cutter
+		/// Wich return a vector of string with a string by dir
+		///
+		/// Example :
+		/// *Project Name*/inc/Item/Potion/Heal
+		/// *Project Name*/src/Item/Potion/Heal
+		/// Into :
+		/// (Considering, std::vector<std::string> v)
+		/// v[0] = *Project Name*
+		/// v[1] = "inc"
+		/// v[2] = "Item"
+		/// v[3] = "Potion"
+		/// v[4] = "Heal"
+		///
+		/// v[0] = *Project Name*
+		/// v[1] = "src"
+		/// v[2] = "Item"
+		/// v[3] = "Potion"
+		/// v[4] = "Heal"
 
-		/// 3 - call state (create a new one for it (for : cpp hpp interface))
-		/// and set the right include for it and inheritance if needed
+		/// 4 - Create dir & file
+		/// Read the vector v (as on the example above), by begining at i = 2
+		/// Create dir for it by following the return value of method of Dir object
+		/// create the file or not
+		/// If the creation of dir doesn t exsit call State object
+		/// to create the file, by sending all the ressources needed
 	}
 	return true;
 }
